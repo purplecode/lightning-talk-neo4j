30c7da30a50d802ecbf6b7e19f6921c78151f1da
purplecode <jaworski.poczta@gmail.com>
2016-02-09 00:17:50 +0100

    slides v0

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,33 @@ var watch = require('gulp-watch');
 
 
 var paths = {
-    src: './src/**/*.jade',
-    dest: './dist/'
+    src: {
+        from: './src/**/*.jade',
+        to: './dist/'
+    },
+    styles: './src/styles/**/*.css',
+    images: {
+        from: './src/images/**/*',
+        to: './dist/images'
+    }
 };
 
-gulp.task('watch', function (cb) {
-    watch(paths.src, function () {
-        gulp.src(paths.src)
+gulp.task('watch', function () {
+
+    watch([paths.src.from, paths.styles], function () {
+        gulp.src(paths.src.from)
             .pipe(jade())
-            .pipe(gulp.dest(paths.dest))
-            .on('end', cb);
+            .pipe(gulp.dest(paths.src.to));
+    });
+
+    watch([paths.images.from], function () {
+        gulp.src(paths.images.from)
+            .pipe(gulp.dest(paths.images.to));
     });
 });
 
 gulp.task('server', function () {
-    gulp.src('.')
+    gulp.src('./dist/')
         .pipe(webserver({
             fallback: 'dist/index.html',
             livereload: true,
